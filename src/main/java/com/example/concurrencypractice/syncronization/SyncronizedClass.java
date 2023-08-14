8c3af220a12e6f2542d27089e37e6cfe4f00d210
shekhar2702 <shekhar.prince1234@gmail.com>
2023-08-14 13:01:35 +0530

    Java concurrency - locking structured and unstructured locks

--- src/main/java/com/example/concurrencypractice/syncronization/SyncronizedClass.java
+++ src/main/java/com/example/concurrencypractice/syncronization/SyncronizedClass.java
@@ -2,8 +2,12 @@ package com.example.concurrencypractice.syncronization;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class ThreadSyncronized implements Runnable {
     private int counter = 0;
+    private Lock l = new ReentrantLock();
     public void increment() {
         try {
 //            Thread.sleep(2000);
@@ -19,15 +23,29 @@ class ThreadSyncronized implements Runnable {
         return counter;
     }
 
-    @Override
-    public void run() {
-        synchronized (this) { //critical section
+//    @Override
+//    public synchronized void run() {//a method can be syncronized and the below can be removed.Here by default the lock which is used is the current object reference.
+//        //syncronization provides mutual exclusion(mutex or one at a time) of critical sections.
+//        synchronized (this) { //critical section
+//            this.increment();
+//            System.out.println(Thread.currentThread().getName() + " increments counter to " + counter);
+//            this.decrement();
+//            System.out.println(Thread.currentThread().getName() + " decrements counter to " + counter);
+//        }
+//    }
+@Override
+public void run() {//using unstructured lock to gain more control.This works similar to above.
+        try {
+            l.lock();
             this.increment();
             System.out.println(Thread.currentThread().getName() + " increments counter to " + counter);
             this.decrement();
             System.out.println(Thread.currentThread().getName() + " decrements counter to " + counter);
         }
+        finally {
+            l.unlock();//since this is an unstructured lock we need to unlock it explicitly unlike in case of syncronous which did it for us automatically.Also its a good practice to write it in finally just in case an exception happens and this block is always ran.In syncronized its handled.
     }
+}
 }
 
 @SpringBootApplication
